afecc0419c389ab7bfba1a36ffa0f0ad8f8caf03
Lysenko Volodymyr <mr.volodumyr@gmail.com>
2026-05-16 23:10:24 +0200

    Fix style violations

--- app/main.py
+++ app/main.py
@@ -21,11 +21,12 @@ sorted_variables = {
         my_favourite_films,
         marks,
         collection_of_coins
-    ],
+   ],
     "immutable": [
         lucky_number,
         pi,
         one_is_a_prime_number,
         name,
-        profile_info],
+        profile_info
+    ],
 }
